ba311be11dd188ea8fb5cc3af323c6d179d9be8e
Maxim Kuzmianok <jst.wrx@gmail.com>
2022-04-18 13:22:33 +0300

    feat: added to navbar avatar and mail

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Image, Nav, Navbar } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import Avatar  from '../../Assets/Images/Avatar.jpg'
 
 const Header = () => {
     return (
@@ -12,6 +13,15 @@ const Header = () => {
                     <Nav className='mr-auto'>
                         <Nav.Link><Link to='/'>Home</Link></Nav.Link>
                         <Nav.Link><Link to='/about'>About me</Link></Nav.Link>
+                        <div>
+                        <Image
+                            roundedCircle
+                            width={100}
+                            height={100}
+                            src={Avatar} />
+                        <h4>Maxim</h4>
+                        <p>jst.wrx@gmail.com</p>
+                        </div>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
